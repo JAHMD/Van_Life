800392f320ef01e95ab8b7ae6a5dd0dea09f6c62
Ahmed Wael <awael3977@gmail.com>
2023-04-24 22:11:08 +0200

    Add main routes to the app

---
+++ src/pages/About.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import bgImg from "../assets/images/about-hero.png";
+
+function About() {
+	return (
+		<section className="about-page-container">
+			<img src={bgImg} className="about-hero-image" />
+			<div className="about-page-content">
+				<h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+				<p>
+					Our mission is to enliven your road trip with the perfect travel van
+					rental. Our vans are recertified before each trip to ensure your
+					travel plans can go off without a hitch. (Hitch costs extra 😉)
+				</p>
+				<p>
+					Our team is full of vanlife enthusiasts who know firsthand the magic
+					of touring the world on 4 wheels.
+				</p>
+			</div>
+			<div className="about-page-cta">
+				<h2>
+					Your destination is waiting.
+					<br />
+					Your van is ready.
+				</h2>
+				<NavLink className="link-button" to="/vans">
+					Explore our vans
+				</NavLink>
+			</div>
+		</section>
+	);
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+function Home() {
+	return (
+		<section className="home-container">
+			<h1>You got the travel plans, we got the travel vans.</h1>
+			<p>
+				Add adventure to your life by joining the #vanlife movement. Rent the
+				perfect van to make your perfect road trip.
+			</p>
+			<NavLink to="vans">Find your van</NavLink>
+		</section>
+	);
+}
+
+export default Home;
